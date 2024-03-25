d8a0d04107fe3089149df80f4cc050e11ca05f70
Kuro <45374906+kuro250104@users.noreply.github.com>
2024-03-25 11:31:40 +0100

    1st TP

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <input type="text" v-model="userName" />
+  <button @click="addUser">Ajouter</button>
+  <ul>
+    <li v-for="user in users">{{user}}</li>
+  </ul>
 </template>
 
+
+<script setup>
+import {ref} from "vue";
+
+const users = ref(['Swann', 'Néo', 'Didou'])
+
+const userName = ref("")
+function addUser(){
+  users.value.push(userName.value);
+  userName.value = "";
+}
+</script>
+
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
